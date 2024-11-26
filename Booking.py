5b1d23676fc42febc06e143ff919b5d71ec77d5f
shintamarito <shintamarito@student.uns.ac.id>
2024-11-26 14:01:22 +0700

    Update Booking.py

--- Booking.py
+++ Booking.py
@@ -10,20 +10,47 @@ def tampilkan_menu():
 def tampilkan_list_penerbangan():
     print("\nKode Penerbangan | Kota Tujuan | Harga Tiket")
     print("------------------------------------------------")
-    print("101              | JAKARTA     | Rp. 1.000.000")
-    print("102              | PONTIANAK   | Rp. 1.300.000")
-    print("103              | MEDAN       | Rp. 1.700.000")
-    print("104              | PAPUA       | Rp. 4.500.000")
-    print("105              | DENPASAR    | Rp. 1.200.000")
+    print("101              | ACEH BESAR              | Rp. 5.000.000")
+    print("102              | DELI SERDANG            | Rp. 4.300.000")
+    print("103              | PADANG PARIAMAN         | Rp. 3.700.000")
+    print("104              | PEKANBARU               | Rp. 4.500.000")
+    print("105              | BATAM KEPULAUAN RIAU    | Rp. 3.200.000")
+    print("106              | TANGGERANG              | Rp. 4.200.000")
+    print("107              | DKI JAKARTA             | Rp. 3.200.000")
+    print("108              | MAJALENGKA              | Rp. 2.200.000")
+    print("109              | YOGYAKARTA              | Rp. 2.300.000")
+    print("110              | SIDOARJO                | Rp. 3.200.000")
+    print("111              | BADUNG                  | Rp. 4.800.000")
+    print("112              | LOMBOK TENGAH           | Rp. 5.200.000")
+    print("113              | BALIKPAPAN              | Rp. 4.500.000")
+    print("114              | SULAWESI SELATAN        | Rp. 4.700.000")
+    print("115              | MANADO                  | Rp. 4.100.000")
+    print("116              | JAYAPURA                | Rp. 6.200.000")
+    print("117              | LABUAN BAJO             | Rp. 5.600.000")
     print("------------------------------------------------")
 
 def get_tujuan_dan_harga(kode_penerbangan):
     tujuan_harga = {
-        "101": ("JAKARTA", 1000000),
-        "102": ("PONTIANAK", 1300000),
-        "103": ("MEDAN", 1700000),
-        "104": ("PAPUA", 4500000),
-        "105": ("DENPASAR", 1200000)
+        "101": ("ACEH BESAR", 5000000),
+        "102": ("DELI SERDANG", 4300000),
+        "103": ("PADANG PARIAMAN", 3700000),
+        "104": ("PEKANBARU", 4500000),
+        "105": ("BATAM KEPULAUAN RIAU", 3200000)
+        "106": ("TANGGERANG", 4200000)
+        "107": ("DKI JAKARTA", 3200000)
+        "108": ("MAJALENGKA", 2200000)
+        "109": ("YOGYAKARTA", 2300000)
+        "110": ("SIDOARJO", 3200000)
+        "111": ("BADUNG", 4800000)
+        "112": ("LOMBOK TENGAH", 5200000)
+        "113": ("BALIKPAPAN", 4500000)
+        "114": ("SULAWESI SELATAN", 4700000)
+        "115": ("MANADO", 4500000)
+        "116": ("JAYAPURA", 6200000)
+        "117": ("LABUAN BAJO", 5600000)
+
+        
+        
     }
     return tujuan_harga.get(kode_penerbangan, ("Kode Salah", 0))
 
